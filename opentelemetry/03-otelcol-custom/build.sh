fef0e496483574979642eb253eb5062edfc740e3
yubo <yubo@yubo.org>
2022-04-09 22:50:01 +0800

    use dockerhub instead of docker build

--- opentelemetry/03-otelcol-custom/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-OUTPUT_PATH=${OUTPUT_PATH:-.}
-
-#go env -w GOPROXY=goproxy.cn,direct
-GO111MODULE=on go install go.opentelemetry.io/collector/cmd/builder@latest
-
-builder --config=./otelcol-builder.yaml --output-path=./.build --name otelcol-custom && \
-mv ./.build/otelcol-custom ${OUTPUT_PATH}
-cp /usr/local/go/lib/time/zoneinfo.zip ${OUTPUT_PATH}/zoneinfo.zip
